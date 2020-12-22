b6c0b081f8ca9bf42b16972e4e107acc99d916d4
Nadeem Douba <ndouba@gmail.com>
2020-12-22 10:20:32 -0500

    Swapped red and green channels in monitor Added plasma.rom from kikko on XMAS-CTF discord #emulator channel

--- src/main/java/ro/htsp/xmas/machine/Monitor.java
+++ src/main/java/ro/htsp/xmas/machine/Monitor.java
@@ -5,15 +5,15 @@ import javafx.scene.shape.Rectangle;
 
 public class Monitor {
     private static final String[] COLOR_MAP = new String[]{
-            "#000000", "#000055", "#0000aa", "#0000ff", "#550000", "#550055", "#5500aa", "#5500ff", "#aa0000",
-            "#aa0055", "#aa00aa", "#aa00ff", "#ff0000", "#ff0055", "#ff00aa", "#ff00ff", "#005500", "#005555",
-            "#0055aa", "#0055ff", "#555500", "#555555", "#5555aa", "#5555ff", "#aa5500", "#aa5555", "#aa55aa",
-            "#aa55ff", "#ff5500", "#ff5555", "#ff55aa", "#ff55ff", "#00aa00", "#00aa55", "#00aaaa", "#00aaff",
-            "#55aa00", "#55aa55", "#55aaaa", "#55aaff", "#aaaa00", "#aaaa55", "#aaaaaa", "#aaaaff", "#ffaa00",
-            "#ffaa55", "#ffaaaa", "#ffaaff", "#00ff00", "#00ff55", "#00ffaa", "#00ffff", "#55ff00", "#55ff55",
-            "#55ffaa", "#55ffff", "#aaff00", "#aaff55", "#aaffaa", "#aaffff", "#ffff00", "#ffff55", "#ffffaa",
-            "#ffffff"
+            "#000000","#000055","#0000aa","#0000ff","#005500","#005555","#0055aa","#0055ff","#00aa00","#00aa55",
+            "#00aaaa","#00aaff","#00ff00","#00ff55","#00ffaa","#00ffff","#550000","#550055","#5500aa","#5500ff",
+            "#555500","#555555","#5555aa","#5555ff","#55aa00","#55aa55","#55aaaa","#55aaff","#55ff00","#55ff55",
+            "#55ffaa","#55ffff","#aa0000","#aa0055","#aa00aa","#aa00ff","#aa5500","#aa5555","#aa55aa","#aa55ff",
+            "#aaaa00","#aaaa55","#aaaaaa","#aaaaff","#aaff00","#aaff55","#aaffaa","#aaffff","#ff0000","#ff0055",
+            "#ff00aa","#ff00ff","#ff5500","#ff5555","#ff55aa","#ff55ff","#ffaa00","#ffaa55","#ffaaaa","#ffaaff",
+            "#ffff00","#ffff55","#ffffaa","#ffffff"
     };
+
     public static Monitor INSTANCE;
     private final Rectangle[] buffer = new Rectangle[64 * 64];
     private final Rectangle[] onScreen = new Rectangle[64 * 64];
